b34452750c6e520f8f7f8f6bab2074ac2d75f64c
DigitalPresales <84257933+DigitalPresales@users.noreply.github.com>
2021-06-06 11:17:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,6 @@
 def registration():
     print("welcome to digital lending platform")
 
-def search():
-    print("here are the search results of digital lender near your area")
-    print("Bank-Of-America is located 3 mins from zip coe 58343")
-    print("Federal Bank is located 4 mins from zip coe 58343")
-    print("Wellsfargo Bank is located 10 mins from zip coe 58343")
-
 if __name__ == '__main__':
     registration()
     search()
