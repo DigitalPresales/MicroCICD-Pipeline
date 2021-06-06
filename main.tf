5db7346c7ed47f2dfce71c3b1c063de0ab95a0c0
DigitalPresales <84257933+DigitalPresales@users.noreply.github.com>
2021-06-06 20:54:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" { 
+  region = "us-west-2"
+  access_key = " Enter your AWS access key here" 
+  secret_key = " Enter your AWS secret key here"
+} 
+
+resource "aws_instance" "Digital Demo" { 
+  
+ ami =  "ami-05d7.....Enter your AMI key here" - You can obtain that from EC2 market place
+ instance_type = "t2.micro"
+  
+  tags= { 
+   Name = "DigitalPreSales Demo EC2 Instance" 
+  }
+  
+}
